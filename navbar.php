857e6540f81c7b20f3ce42a3e89ff5dd2bf81134
Wesley708 <65147671+Wesley708@users.noreply.github.com>
2023-09-24 11:52:35 -0300

    Mudei a estética da página de login

--- navbar.php
+++ navbar.php
@@ -1,15 +1,12 @@
 
-
 <div class="hero">
-        <nav>
-            <a class="navbar-brand" href="#">
-                <img src="logo.png" alt="" width="100" height="60">
-              </a>
-              <?php
-          echo "<h3 id='usuario'>Bem Vindo $logado</h3>";
-          ?>
-            <a href="sair.php" class="btns"> Sair</a>
-        </nav>
-    </div>
-
-    
+    <nav>
+        <a class="navbar-brand" href="#">
+            <img src="logo.png" alt="" width="100" height="60">
+            </a>
+            <?php
+        echo "<h3 id='usuario'>Bem Vindo $logado</h3>";
+        ?>
+        <a href="sair.php" class="btns"> Sair</a>
+    </nav>
+</div>
